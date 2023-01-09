0e69c67370c4a00b84db4abe84ed954947c94933
Annasauliarahman <annascahwetan@gmail.com>
2023-01-09 12:59:35 +0700

    Memasukan file

---
+++ asset/script.js
@@ -0,0 +1,2 @@
+import FortawesomeFontawesomeFree from "https://cdn.skypack.dev/@fortawesome/fontawesome-free@6.2.1";
+import FortawesomeFontawesomeFree from "https://cdn.skypack.dev/@fortawesome/fontawesome-free@6.2.1";
